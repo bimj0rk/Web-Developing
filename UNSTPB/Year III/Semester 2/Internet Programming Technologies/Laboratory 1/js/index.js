973f5fce264d08f4ed4f4ea179e73c2aa8859aa0
Adrian Câmpean <37622282+bimj0rk@users.noreply.github.com>
2024-02-29 19:43:38 +0200

    lab1 p2

--- UNSTPB/Year III/Semester 2/Internet Programming Technologies/Laboratory 1/js/index.js
+++ UNSTPB/Year III/Semester 2/Internet Programming Technologies/Laboratory 1/js/index.js
@@ -1,43 +1,68 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const form = document.getElementById("registration");
-    const name = document.getElementById("name");
-    const user = document.getElementById("user");
-    const password = document.getElementById("password");
-    const confirm = document.getElementById("confirm");
-    const email = document.getElementById("email");
-    const phone = document.getElementById("phone");
-    const gender = document.querySelectorAll("gender");
+function CheckIfGood(){
+    const name = document.getElementById("name").value;
+    const user = document.getElementById("user").value;
+    const password = document.getElementById("password").value;
+    const confirm = document.getElementById("confirm").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
 
-    form.addEventListener("submit", function(event){
-        let error = [];
+    if(name === ""){
+        alert("Name musn't be empty!");
+        return false;
+    }
 
-        if(name.value === "") error.push("Name musn't be empty!");
-        if(user.value === "") error.push("Username musn't be empty!");
-        if(password.value === "") error.push("Password musn't be empty!");
-        if(confirm.value === "") error.push("Confirm password musn't be empty!");
-        if(email.value === "") error.push("Email musn't be empty!");
-        if(phone.value === "") error.push("Phone no. musn't be empty!");
+    if(user === ""){
+        alert("Username musn't be empty!");
+        return false;
+    }
 
-        if(password.value !== confirm.value) error.push("Passwords MUST match.");
+    if(password === ""){
+        alert("Password musn't be empty!");
+        return false;
+    }
 
-        let genderIsSelected = false;
-        gender.forEach(input => {
-            if(input.checked){
-                genderIsSelected = true;
-            }
-        });
+    if(confirm === ""){
+        alert("Confirm password musn't be empty!");
+        return false;
+    }
 
-        if(!genderIsSelected) error.push("Select a gender, you cannot be genderless you bafoon.");
+    if(password !== confirm){
+        alert("Passwords MUST match.");
+        return false;
+    }
 
-        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-        if(emailRegex.test(email.value)) error.push("Email is not an email you blithering idiot.");
+    if(email === ""){
+        alert("Email musn't be empty!");
+        return false;
+    }
 
-        const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
-        if(phoneRegex.test(phone.value)) error.push("Phone no. does not seem to be in a phone no. shape, discombobulated monkey.");
+    if(phone === ""){
+        alert("Phone no. musn't be empty!");
+        return false
+    }
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if(emailRegex.test(email.value)){
+        alert("Email is not an email.");
+        return false;
+    }
 
-        if(error.length > 0){
-            event.preventDefault();
-            alert(error.join("\n"));
+    const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+    if(phoneRegex.test(phone.value)){
+        alert("Phone number does not seem to be in a phone number shape.");
+        return false;
+    }
+
+    let genderIsSelected = false;
+    gender.forEach(input => {
+        if(input.checked){
+            genderIsSelected = true;
         }
     });
-});
+
+    if(!genderIsSelected){
+        alert("Select a gender.");
+        return false;
+    } 
+    
+    return true;
+}
