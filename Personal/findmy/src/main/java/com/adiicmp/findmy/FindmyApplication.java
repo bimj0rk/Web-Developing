e933e094a2ca35a36d7a6de8e66a8f3fad3f226e
Adrian Câmpean <aicampean@gmail.com>
2024-09-26 20:23:47 +0300

    dvdsfgbvsfde

--- Personal/findmy/src/main/java/com/adiicmp/findmy/FindmyApplication.java
+++ Personal/findmy/src/main/java/com/adiicmp/findmy/FindmyApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
@@ -11,5 +15,18 @@ import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConf
 public class FindmyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FindmyApplication.class, args);
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory servletContainer(){
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		Connector ajpConnector = new Connector("AJP/1.3");
+		ajpConnector.setPort(9091);
+		ajpConnector.setSecure(false);
+		ajpConnector.setAllowTrace(false);
+		ajpConnector.setScheme("http");
+		((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+		tomcat.addAdditionalTomcatConnectors(ajpConnector);
+		return tomcat;
 	}
 }
